0397e4803e579403dfca90e44e445830628be13d
Ricardo Piras <ricpiras@gmail.com>
2018-12-29 04:45:37 +0900

    Home page is working

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -1,8 +1,8 @@
 class Designer < ApplicationRecord
  # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # devise :database_authenticatable, :registerable,
+  #        :recoverable, :rememberable, :trackable, :validatable
   has_many :collections, dependent: :destroy
   has_many :looks, dependent: :destroy
   has_many :products, dependent: :destroy
