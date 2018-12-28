ae7386fc36a40ed8eccd6a6eb84e6d1c8bb299d0
Ricardo Piras <ricpiras@gmail.com>
2018-12-28 20:20:58 +0900

    Fixed designer model

--- app/models/designer.rb
+++ app/models/designer.rb
@@ -4,5 +4,9 @@ class Designer < ApplicationRecord
   has_many :collections, dependent: :destroy
   has_many :looks, dependent: :destroy
   has_many :products, dependent: :destroy
+  has_and_belongs_to_many :categories
+  has_attachment :profile_photo
 
+  geocoded_by :city
+  after_validation :geocode, if: :city_changed?
 end
