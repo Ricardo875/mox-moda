d46f140935ea386bfab688931ce5fe7f397ea726
Ricardo Piras <ricpiras@gmail.com>
2018-12-28 20:29:42 +0900

    controlers created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Creating initial seed"
+OrderProduct.destroy_all
+Category.destroy_all
+Designer.destroy_all
+StoreCollection.destroy_all
+Retailer.destroy_all
+Product.destroy_all
+Collection.destroy_all
+
+Category.create!(
+  [
+    {title: "Womenswear"},
+    {title: "Menswear"},
+    {title: "Outerwear"},
+    {title: "Footwear"},
+    {title: "Active"},
+    {title: "Hats"},
+    {title: "Active"},
+    {title: "Bags"},
+    {title: "Beachwear"},
+    {title: "Jewellery"},
+    {title: "Eyewear"},
+    {title: "Accessories"}
+  ]
+)
