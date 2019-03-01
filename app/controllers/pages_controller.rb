e08f6475168b15903c6e74391ed81d41df4149a8
Ricardo Piras <ricpiras@gmail.com>
2019-03-01 09:36:50 +0900

    Commenting out hashes of page controller for debugging.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,21 +1,21 @@
 class PagesController < ApplicationController
 
   def home
-    @designers = Designer.where.not(latitude: nil, longitude: nil)
+    @designers = Designer
 
     @hash = Gmaps4rails.build_markers(@designers) do |designer, marker|
-      marker.lat designer.latitude
-      marker.lng designer.longitude
+      # marker.lat designer.latitude
+      # marker.lng designer.longitude
       # marker.infowindow render_to_string(partial: "/flats/map_box", locals: { flat: flat })
-      marker.infowindow "<div class='marker-card'> <a class='marker-link' href='#{designer_path(designer)}'>
-      <img src='#{designer.profile_photo.fullpath}' alt='Profile picture' class='marker-image'/>
-      <br> <h4 class='marker-text'>#{designer.brand}</h4> </a>
-      </div>"
-      marker.picture({
-        url: "#{view_context.image_path 'favicon-16x16.png'}",
-        width: 32,
-        height: 32
-        })
+      # marker.infowindow "<div class='marker-card'> <a class='marker-link' href='#{designer_path(designer)}'>
+      # <img src='#{designer.profile_photo.fullpath}' alt='Profile picture' class='marker-image'/>
+      # <br> <h4 class='marker-text'>#{designer.brand}</h4> </a>
+      # </div>"
+      # marker.picture({
+      #   url: "#{view_context.image_path 'favicon-16x16.png'}",
+      #   width: 32,
+      #   height: 32
+      #   })
 
     end
 end
