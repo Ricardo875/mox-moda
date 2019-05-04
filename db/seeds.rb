6070d0a6324cb0ff047c54f3f00ad7fc74be8955
Ricardo Piras <ricpiras@gmail.com>
2019-05-05 07:15:20 +0900

    Slight seeds update to bug fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Creating initial seed"
-OrderProduct.destroy_all
-Category.destroy_all
-Designer.destroy_all
-StoreCollection.destroy_all
-Retailer.destroy_all
-Product.destroy_all
-Collection.destroy_all
+# puts "Creating initial seed"
+# OrderProduct.destroy_all
+# Category.destroy_all
+# Designer.destroy_all
+# StoreCollection.destroy_all
+# Retailer.destroy_all
+# Product.destroy_all
+# Collection.destroy_all
 
 Category.create!(
   [
